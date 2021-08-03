ddae5dd79009f30d7f00df6b4ca9e0dad768c0e9
Dane Petersen <dane.m.petersen@gmail.com>
2021-08-03 16:59:06 -0500

    added tables in postico and added queries to database.sql

--- database.sql
+++ database.sql
@@ -3,3 +3,11 @@ CREATE TABLE "artists" (
     "artist_name" varchar(80) not null,
     "year_born" date
 );
+
+CREATE TABLE "song" (
+	-- Define my columns <-- sql comment
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR(255), -- varchar is a simple way of saying just text/string
+	"length" VARCHAR(10),
+	"released" DATE
+);
